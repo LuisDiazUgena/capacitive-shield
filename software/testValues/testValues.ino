0d03e1f38cd4bdca092934c494b0b2bd7c4f2e2d
Luis Diaz <luisdiazugena@gmail.com>
2015-09-16 12:07:41 +0200

    removed unnecesary code

--- software/testValues/testValues.ino
+++ software/testValues/testValues.ino
@@ -10,9 +10,9 @@ int samples = 10;
 int pinLed = 7;
 
 void setup(){
-   //capBtn.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
    initialValue = capBtn.capacitiveSensor(samples);
+   pinMode(pinLed, OUTPUT);
 }
 
 void loop(){
@@ -20,14 +20,12 @@ void loop(){
 
     long value =  capBtn.capacitiveSensor(samples);
 
-    Serial.print("\t");                    // tab character for debug windown spacing
-
     if(value > (initialValue + threshold)){
       digitalWrite(pinLed,HIGH);
     }else{
       digitalWrite(pinLed, LOW);
     }
-    Serial.println(value);                  // print sensor output 2
+    Serial.println(value);                  // print sensor output
 
     delay(100);                             // arbitrary delay to limit data to serial port
 }
